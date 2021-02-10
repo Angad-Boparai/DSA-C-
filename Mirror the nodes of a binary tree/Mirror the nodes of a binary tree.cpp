e158822193211aa20ca5ead649d21dca75db3379
Angad-Boparai <61798473+Angad-Boparai@users.noreply.github.com>
2021-02-10 22:49:19 +0530

    Add files via upload

---
+++ Mirror the nodes of a binary tree/Mirror the nodes of a binary tree.cpp
@@ -0,0 +1,36 @@
+/**********************************************************
+	Following is the Binary Tree Node class structure
+
+	template <typename T>
+	class BinaryTreeNode {
+    	public : 
+    	T data;
+    	BinaryTreeNode<T> *left;
+    	BinaryTreeNode<T> *right;
+
+    	BinaryTreeNode(T data) {
+        	this -> data = data;
+        	left = NULL;
+        	right = NULL;
+    	}
+	};
+
+***********************************************************/
+
+void mirrorBinaryTree(BinaryTreeNode<int>* root) {
+    // Write your code here
+    if(root==NULL){
+        return;
+    }
+    
+   
+    
+    mirrorBinaryTree(root->left);
+	    
+    mirrorBinaryTree(root->right);
+    
+    BinaryTreeNode<int>* temp1=root->left;
+    root->left=root->right;
+    root->right=temp1;
+    
+}
