830349bfb882184d1196c4cf495bfc0dead34306
Angad-Boparai <61798473+Angad-Boparai@users.noreply.github.com>
2021-03-20 23:49:21 +0530

    Update Find the Knapsack Equal Partition Subset Sum.cpp

--- Find the equal partition subset sum/Find the Knapsack Equal Partition Subset Sum.cpp
+++ Find the equal partition subset sum/Find the Knapsack Equal Partition Subset Sum.cpp
@@ -9,38 +9,16 @@ int equalPartition(int arr[], int N){
 		sum=sum+arr[i];
 	}
 	
-	int t[N+1][sum+1];
 	
-	for(int i=0;i<N+1;i++){
-		for(int j=0;i<sum+1;j++){
-		
-			if(i==0){
-				t[i][j]=0;
-			}
-			
-			if(j==0){
-				t[i][j]=1;
-			}
-		}
+	if(sum%2!=0){
+	   return false;
 	}
-	
-	for(int i=0;i<N+1;i++){
-		for(int j=0;i<sum+1;j++){
+			
+	else{
+	    return subset(arr,N,sum/2);
+	 }	
+			
 		
-		if(sum%2!=0){
-			return false;
-			}
-			
-		else{
-			return subset(arr,N,sum/2);
-		}	
-			
-		}
-	}
-	
-	
-	
-	
 }
 
 
