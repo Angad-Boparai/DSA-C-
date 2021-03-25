83b760384365bc02d6725f0bfc245703cf52fdd1
Angad-Boparai <61798473+Angad-Boparai@users.noreply.github.com>
2021-03-25 19:09:27 +0530

    Add files via upload

---
+++ Implementation of Graph/Implementation of Graph.cpp
@@ -0,0 +1,56 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+void print(int *edges,int n, int sv,int *visited){
+    
+    cout<<sv;
+    visited[sv]=true;
+    
+    for(int i=0;i<n;++){
+        if(i==sv){
+            continue;
+        }
+        if(edges[sv][i]==1){
+            if(visited[i]=true){
+                continue;
+                }
+                
+            print(edges,n,i,visited)    
+        }
+        
+    }
+}
+
+int main()
+    {
+    int n;
+    int e;
+    cin>>n>>e;
+    
+    int **edges=new int*[n];
+    
+    for(int i=0;i<n;i++){
+        edges[i]=new int[n];
+        for(int j=0;j<n;j++){
+            edges[i][j]=0;
+        }
+    }
+    
+    
+    for(int i=0;i<e;i++){
+        int f;
+        int s;
+        cin>>f>>s;
+        
+        edges[f][s]=1;
+        edges[s][f]=1;
+    }
+    
+    bool* visited=new bool[n];
+    
+    for(int i=0;i<n;i++){
+        visited[i]=false;
+    }
+    void print(*edges,n,0,*visited);
+}
