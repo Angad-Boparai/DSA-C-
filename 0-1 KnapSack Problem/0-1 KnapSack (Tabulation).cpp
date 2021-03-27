42a4316e6430df665d66150f7e07ca15632fbf65
Angad-Boparai <61798473+Angad-Boparai@users.noreply.github.com>
2021-03-27 12:10:44 +0530

    Update 0-1 KnapSack (Tabulation).cpp

--- 0-1 KnapSack Problem/0-1 KnapSack (Tabulation).cpp
+++ 0-1 KnapSack Problem/0-1 KnapSack (Tabulation).cpp
@@ -17,8 +17,8 @@ for(int i=0;i<1001;i++){
 
 int KnapSack(int val[],int wt[],int n, int W){
 	
-	for(int i=0;i<n;i++){
-		for(int j=0;i<W;j++){
+	for(int i=1;i<n+1;i++){
+		for(int j=1;i<W+1;j++){
 			
 			if(wt[i-1]<=j){
 				t[i][j]=max(val[i-1] + t[i-1][j-wt[i-1]],t[i-1][j]);
